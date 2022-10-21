8b83934d0138085a3b1f7953311ebdd1f36840ae
mkd92 <manikandan.m1116@gmail.com>
2022-10-21 21:13:30 +0530

    Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
-import Login from "./components/Login";
+import UserList from "./features/users/UserList";
+
+import NotesList from "./features/notes/NotesList";
+
+import Welcome from "./features/auth/Welcome";
+
+import DashLayout from "./components/DashLayout";
+
+import Login from "./features/auth/Login";
 
 import Public from "./components/Public";
 
@@ -12,6 +20,16 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UserList />} />
+          </Route>
+        </Route>
+        {/*end Dash*/}
       </Route>
     </Routes>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { Provider } from "react-redux";
+
+import store from "./app/store";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ReactDOM from "react-dom/client";
@@ -9,11 +13,13 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
